c10fd3ac5c453950f2038b25af7564c9b3541346
ani <you@example.com>
2024-06-23 23:58:31 +0530

    okidoki

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import {
-  BrowserRouter as Router,
+  RouterProvider,
   Route,
   Routes,
   useNavigate,
   useLocation,
   useBlocker,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Link,
+  Outlet,
 } from "react-router-dom";
 import { confirmDialog } from "primereact/confirmdialog";
 import { ConfirmDialog } from "primereact/confirmdialog";
@@ -18,63 +22,52 @@ const Page1 = () => <div>Page1</div>;
 const Page2 = () => <div>Page2</div>;
 
 const BlockNavigation = () => {
-  const [lastLocation, setLastLocation] = useState(null);
-  const [confirmNavigation, setConfirmNavigation] = useState(false);
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  let blocker = useBlocker(
-    ({ currentLocation, nextLocation }) =>
-      currentLocation.pathname !== nextLocation.pathname
-  );
-
-  // useEffect(() => {
-  //   const handleWindowClose = (e) => {
-  //     e.preventDefault();
-  //     e.returnValue = "Do you really want to go back?";
-  //     setConfirmNavigation(true);
-  //     setLastLocation(location);
-  //     return "asdf";
-  //   };
-
-  //   window.addEventListener("beforeunload", handleWindowClose);
-  //   return () => {
-  //     window.removeEventListener("beforeunload", handleWindowClose);
-  //   };
-  // }, []);
+  let blocker = useBlocker(true);
 
   useEffect(() => {
-    if (confirmNavigation) {
+    if (blocker.state === "blocked") {
       confirmDialog({
         message: "Do you really want to go back?",
         header: "Confirmation",
         icon: "pi pi-exclamation-triangle",
         accept: () => {
-          setConfirmNavigation(false);
-          navigate(-1);
+          blocker.proceed();
+          // navigate(-1);
         },
         reject: () => {
-          setConfirmNavigation(false);
+          blocker.reset();
         },
       });
     }
-  }, [confirmNavigation]);
+  }, [blocker.state]);
 
   return null;
 };
 
-const App = () => {
+const Layout = () => {
   return (
-    <Router>
+    <>
       <ConfirmDialog />
       <BlockNavigation />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/page1" exact element={<Page1 />} />
-        <Route path="/page2" exact element={<Page2 />} />
-      </Routes>
-    </Router>
+      <Link to="/home">home</Link>
+      <Link to="/page1">page1</Link>
+      <Link to="/page2">page2</Link>
+      <Outlet />
+    </>
   );
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="page1" element={<Page1 />} />
+      <Route path="page2" element={<Page2 />} />
+    </Route>
+  )
+);
+const App = () => {
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
